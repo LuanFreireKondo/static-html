e0494d6c013f35f9fd2c5ca39ff06292b98cca08
LuanFreireKondo <luanfreirekondo@gmail.com>
2024-12-18 12:11:37 -0300

    remove GTM

--- src/javascript/sw.js
+++ src/javascript/sw.js
@@ -1,76 +1,72 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js').then((registration) => {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (err) => {
-      // registration failed
-      console.log('ServiceWorker registration failed:', err)
-    })
-  })
-}
+const version = '0.0.1'
 
-var CACHE_NAME = 'static-html-v1.0.0'
-var urlsToCache = [
-  '/home.js',
-  '/index.html',
-  '/assets/style.css',
-  '/assets/logo-192x192.png',
-  '/assets/logo-512x512.png',
-  '/assets/manifest.json',
-]
+const CACHE_NAME = version
+const URLS_TO_CACHE = ['style.css']
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-    .then((cache) => {
-      console.log('Opened cache')
-      return cache.addAll(urlsToCache)
-    })
+      .then(function(cache) {
+        console.log('Opened cache')
+        return cache.addAll(URLS_TO_CACHE)
+      })
   )
 })
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim())
-})
+// self.addEventListener('activate', event => {
+//   // delete any caches that aren't in expectedCaches
+//   event.waitUntil(
+//     caches.keys().then(keys => Promise.all(
+//       keys.map(key => {
+//         if (!CACHE_NAME.includes(key)) {
+//           return caches.delete(key)
+//         }
+//       })
+//     )).then(() => {
+//       console.log(`ServiceWorker v${CACHE_NAME} now ready to handle fetches!`)
+//       console.log('S E R V I C E  W O R K E R')
+//     })
+//   );
+// })
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response
-        }
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response
+//         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone()
+//         // IMPORTANT: Clone the request. A request is a stream and
+//         // can only be consumed once. Since we are consuming this
+//         // once by cache and once by the browser for fetch, we need
+//         // to clone the response.
+//         const fetchRequest = event.request.clone()
 
-        return fetch(fetchRequest).then(
-          (response) => {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response
-            }
+//         return fetch(fetchRequest).then(
+//           function(response) {
+//             // Check if we received a valid response
+//             if(!response || response.status !== 200 || response.type !== 'basic') {
+//               return response
+//             }
 
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone()
+//             // IMPORTANT: Clone the response. A response is a stream
+//             // and because we want the browser to consume the response
+//             // as well as the cache consuming the response, we need
+//             // to clone it so we have two streams.
+//             const responseToCache = response.clone()
 
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache)
-              })
+//             caches.open(CACHE_NAME)
+//               .then(function(cache) {
+//                 cache.put(event.request, responseToCache)
+//               })
 
-            return response
-          }
-        )
-      })
-    )
-})
+//             return response
+//           }
+//         )
+//       })
+//   )
+// })
+
